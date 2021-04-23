303403b4adf411c3505d12c0fb1f462061ca3f01
Victoria Kee <victoriakee95@gmail.com>
2021-04-23 11:11:17 -0500

    Add comments/show work

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
-  
+//create array of colors to access
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+//grab button element
 const btn = document.getElementById("btn");
+//grab color class
 const color = document.querySelector(".color");
 
+//create event listener to run on click of button
 btn.addEventListener("click", function () {
-  const randomNumber = getRandomNumber();
+    //get randNum for array index
+    const randomNumber = getRandomNumber();
 
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+    //change background color to color at random index
+    document.body.style.backgroundColor = colors[randomNumber];
+    //list the selected color for user
+    color.textContent = colors[randomNumber];
 });
 
 function getRandomNumber() {
--- backend/hex.js
+++ backend/hex.js
@@ -1,14 +1,22 @@
+//create array of all possible hex num/letters
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+//grab button
 const btn = document.getElementById("btn");
+//grab color class
 const color = document.querySelector(".color");
 
+//create function to run on button click
 btn.addEventListener("click", function () {
+  //hold spot for hex code with start hash
   let hexColor = "#";
+  //run for loop to get six random values from the hex array
   for (let i = 0; i < 6; i++) {
+      //the value of hexColor is equal to itself plus another (random) num from the array
     hexColor += hex[getRandomNumber()];
   }
-
+  //show the num/letter value of the hex color
   color.textContent = hexColor;
+  //make page the color of our hexColor
   document.body.style.backgroundColor = hexColor;
 });
 
